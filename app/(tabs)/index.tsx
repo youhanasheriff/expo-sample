1c5f5532f6ce64d5be9744ff8f0d44ef1be4b0a2
Youhana Sheriff <youhanasheriff@gmail.com>
2024-05-14 15:30:09 +0530

    refactor: Improve home screen animation with rotation effect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,39 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Image, StyleSheet, Text, View, Animated, Easing } from 'react-native';
 
 export default function HomeScreen() {
+  const progress = new Animated.Value(0);
+
+  useEffect(() => {
+    
+    Animated.loop(
+      Animated.timing(progress, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: true,
+        easing: Easing.linear,
+      })
+    ).start();
+
+    return () => {
+      progress.stopAnimation();
+    }
+  }, [])
+
+  const rotate = progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+  
   return (
     <View style={styles.stepContainer}>
-      <View style={styles.titleContainer}>
+      <Animated.View style={[styles.titleContainer, { transform: [{ rotate: rotate }]}]}>
         <Image
           source={require('@/assets/images/react-logo.png')}
           style={styles.reactLogo}
+          resizeMode='contain'
           />
-      </View>
+      </Animated.View>
       <Text>Open up the code for this screen:</Text>
     </View>
   );
@@ -18,21 +43,17 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
     width: '100%',
     height: '20%',
-    gap: 8,
   },
   stepContainer: {
     flex: 1,
     alignItems: 'center',
-    gap: 8,
-    marginBottom: 8,
+    justifyContent: 'center',
   },
   reactLogo: {
     height: 178,
     width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
